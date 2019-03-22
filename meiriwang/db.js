47c7de576ec7230fb250748ec409249081c6353b
easterCat <fuhuodemao@163.com>
2019-03-22 10:17:20 +0800

    修复一些问题

--- meiriwang/db.js
+++ meiriwang/db.js
@@ -3,41 +3,56 @@ var fs = require("fs");
 var path_dir = "D:\\data\\wwwroot\\xiezhenji.web\\static\\mrw";
 
 var connection = mysql.createConnection({
-    host: "192.168.0.164",
-    port: "3306",
-    user: "xiezhenji",
-    password: "iJAuzTbdrDJDswjPN6!*M*6%Ne",
-    database: "xiezhenji"
+  host: "192.168.0.164",
+  port: "3306",
+  user: "xiezhenji",
+  password: "iJAuzTbdrDJDswjPN6!*M*6%Ne",
+  database: "xiezhenji"
 });
 
 connection.connect();
 
 let files = fs.readdirSync(path_dir, {
-    encoding: "utf-8"
+  encoding: "utf-8"
 });
 
 files.forEach(async (file, index) => {
-    var photo_files = fs.readdirSync(`${path_dir}\\${file}`, {
-        encoding: "utf-8"
-    });
+  var photo_files = fs.readdirSync(`${path_dir}\\${file}`, {
+    encoding: "utf-8"
+  });
 
-    var length = photo_files.length;
+  var length = photo_files.length;
 
-    var first_name = photo_files[0];
+  var first_name = photo_files[0];
 
-    var cover_img_path = `/mrw/${file}/${first_name}`;
+  var cover_img_path = `/mrw/${file}/${first_name}`;
 
-    var new_name = `mrw_${index + 1}`;
+  var new_name = `mrw_${index + 1}`;
 
-    // var sql = `UPDATE photo_album_collect SET num=?`;
-    // var sql_params = [length];
+  // var sql = `UPDATE photo_album_collect SET num=?`;
+  // var sql_params = [length];
 
-    update([length, new_name]);
+  update([length, new_name]);
 
-    // var sql = `INSERT INTO photo_album_collect(tags,name,num,intro,cover_img,dir) VALUES(?,?,?,?,?,?)`;
-    // var sql_params = ["美女", file, Number(photo_files.length), file, cover_img_path, `mrw/${file}`];
+  // var sql = `INSERT INTO photo_album_collect(tags,name,num,intro,cover_img,dir) VALUES(?,?,?,?,?,?)`;
+  // var sql_params = ["美女", file, Number(photo_files.length), file, cover_img_path, `mrw/${file}`];
 
-    //connection.query(sql, sql_params, function(err, result) {
+  //connection.query(sql, sql_params, function(err, result) {
+  // if (err) {
+  // console.log("[SELECT ERROR] - ", err.message);
+  //     return;
+  // }
+  // console.log("--------------------------SELECT----------------------------");
+  // console.log(result);
+  // console.log("------------------------------------------------------------\n\n");
+  //});
+});
+
+function update(arr) {
+  var sql = `UPDATE photo_album_collect SET num = ? WHERE new_name = ?`;
+  var sql_params = arr;
+
+  connection.query(sql, sql_params, function(err, result) {
     // if (err) {
     // console.log("[SELECT ERROR] - ", err.message);
     //     return;
@@ -45,22 +60,7 @@ files.forEach(async (file, index) => {
     // console.log("--------------------------SELECT----------------------------");
     // console.log(result);
     // console.log("------------------------------------------------------------\n\n");
-    //});
-});
-
-function update(arr) {
-    var sql = `UPDATE photo_album_collect SET num = ? WHERE new_name = ?`;
-    var sql_params = arr;
-
-    connection.query(sql, sql_params, function(err, result) {
-        // if (err) {
-        // console.log("[SELECT ERROR] - ", err.message);
-        //     return;
-        // }
-        // console.log("--------------------------SELECT----------------------------");
-        // console.log(result);
-        // console.log("------------------------------------------------------------\n\n");
-    });
+  });
 }
 
 connection.end();
--- meiriwang/updateTag.js
+++ meiriwang/updateTag.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "192.168.0.164",
-    port: "3306",
-    user: "xiezhenji",
-    password: "iJAuzTbdrDJDswjPN6!*M*6%Ne",
-    database: "xiezhenji"
+  host: "192.168.0.164",
+  port: "3306",
+  user: "xiezhenji",
+  password: "iJAuzTbdrDJDswjPN6!*M*6%Ne",
+  database: "xiezhenji"
 });
 
 connection.connect();
@@ -13,61 +13,61 @@ connection.connect();
 init();
 
 async function init() {
-    try {
-        let all_tag = await selectTag();
-        all_tag = all_tag.map(i => i.name);
+  try {
+    let all_tag = await selectTag();
+    all_tag = all_tag.map(i => i.name);
 
-        let all_collect = await selectCollect();
+    let all_collect = await selectCollect();
 
-        all_collect.forEach(element => {
-            let name = element.name;
-            let arr = [];
-            if (name) {
-                all_tag.forEach(tag => {
-                    let reg = new RegExp(`${tag}`);
-                    if (reg.test(name)) {
-                        if (arr.length > 5) return;
-                        arr.push(tag);
-                    }
-                });
-                updateCollect([arr.join(","), element.id]);
-            }
+    all_collect.forEach(element => {
+      let name = element.name;
+      let arr = [];
+      if (name) {
+        all_tag.forEach(tag => {
+          let reg = new RegExp(`${tag}`);
+          if (reg.test(name)) {
+            if (arr.length > 5) return;
+            arr.push(tag);
+          }
         });
-        connection.end();
-    } catch (err) {
-        console.log(err);
-    }
+        updateCollect([arr.join(","), element.id]);
+      }
+    });
+    connection.end();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function selectTag() {
-    let sql = "SELECT name FROM photo_tag_list";
+  let sql = "SELECT name FROM photo_tag_list";
 
-    return new Promise((resolve, reject) => {
-        connection.query(sql, function(err, result) {
-            if (err) {
-                reject("[SELECT ERROR] - ", err.message);
-            }
-            resolve(result);
-        });
+  return new Promise((resolve, reject) => {
+    connection.query(sql, function(err, result) {
+      if (err) {
+        reject("[SELECT ERROR] - ", err.message);
+      }
+      resolve(result);
     });
+  });
 }
 
 async function selectCollect() {
-    let sql = "SELECT id,name FROM photo_album_collect";
+  let sql = "SELECT id,name FROM photo_album_collect";
 
-    return new Promise((resolve, reject) => {
-        connection.query(sql, function(err, result) {
-            if (err) {
-                reject("[photo_album_collect SELECT ERROR] - ", err.message);
-            }
-            resolve(result);
-        });
+  return new Promise((resolve, reject) => {
+    connection.query(sql, function(err, result) {
+      if (err) {
+        reject("[photo_album_collect SELECT ERROR] - ", err.message);
+      }
+      resolve(result);
     });
+  });
 }
 
 async function updateCollect(arr) {
-    var sql = `UPDATE photo_album_collect SET tags = ? WHERE id = ?`;
-    var sql_params = arr;
+  var sql = `UPDATE photo_album_collect SET tags = ? WHERE id = ?`;
+  var sql_params = arr;
 
-    connection.query(sql, sql_params, function(err, result) {});
+  connection.query(sql, sql_params, function(err, result) {});
 }
